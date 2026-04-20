d4550f366c743fd410cf31cc7acf02f07c8f401b
loser-2-o <muhammadanamurrashid@gmail.com>
2026-04-21 03:46:48 +0600

    projects

---
+++ 06currencycoverter/src/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // এই লাইনটি খুব গুরুত্বপূর্ণ
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
